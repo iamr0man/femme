e57ae4449da8d65afee6d448b94494e58b1e7147
iamr0man <koriny4@gmail.com>
2020-04-14 19:25:44 +0300

    init app

---
+++ femme/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    Femme
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style lang="scss">
+
+@font-face {
+  font-family: "Femme";
+  src: url('./assets/fonts/femme.ttf')
+}
+
+#app {
+  font-family: "Femme", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
